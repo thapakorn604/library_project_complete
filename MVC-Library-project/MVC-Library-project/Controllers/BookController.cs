5e5bed23fa2d2b823bcedfff2af55a0238b2ffd3
Piyawasin Pikror-ngarn <piyawasin@gmail.com>
2017-11-26 10:32:48 +0700

    Controllers created.
    Controllers created. Implementation is still on its way.

---
+++ MVC-Library-project/MVC-Library-project/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLibraryproject.Abstract;
+using MVCLibraryproject.Models;
+
+namespace MVCLibraryproject.Controllers
+{
+    public class BookController : Controller
+    {
+      
+    }
+}
