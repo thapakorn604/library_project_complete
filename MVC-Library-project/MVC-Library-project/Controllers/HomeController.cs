83d1ef6fa5f2032591e665fd15136ddc8aec7931
Piyawasin Pikror-ngarn <piyawasin@gmail.com>
2017-11-26 10:22:29 +0700

    Remove useless pre-defined ViewData.
    Remove useless pre-defined ViewData in HomeController at Index().

--- MVC-Library-project/MVC-Library-project/Controllers/HomeController.cs
+++ MVC-Library-project/MVC-Library-project/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace MVCLibraryproject.Controllers
     {
         public ActionResult Index()
         {
-            var mvcName = typeof(Controller).Assembly.GetName();
-            var isMono = Type.GetType("Mono.Runtime") != null;
-
-            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
-
             return View();
         }
     }
