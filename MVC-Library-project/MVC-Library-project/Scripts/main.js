c3c0392c3967f4ad37a3db62d91cbefff9f0f968
ploychanok <plo_y@live.com>
2017-11-30 01:32:21 +0700

    Book-Model
    Final Version for book Model!

--- MVC-Library-project/MVC-Library-project/Scripts/main.js
+++ MVC-Library-project/MVC-Library-project/Scripts/main.js
@@ -9,8 +9,7 @@
       alignment: 'left', // Displays dropdown with edge aligned to the left of button
       stopPropagation: true // Stops event propagation
     }
-
   );
-});
+   $('.modal-trigger').leanModal(); //ฟวยยยย คำสั่งชาติไหนแล้ว ชาวบ้านเขาไป 1.0 รีบอัพเดทได้ล้าววว ถถถถถถ
 
- 
+});
