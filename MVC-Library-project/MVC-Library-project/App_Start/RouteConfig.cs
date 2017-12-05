84db678dd548f4b86b096ee2215f0edf7358f057
ploychat <nuntachat_pat@cmu.ac.th>
2017-12-05 16:00:59 +0700

    librarain page

--- MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
+++ MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MVCLibraryproject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Member", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Librarian", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
