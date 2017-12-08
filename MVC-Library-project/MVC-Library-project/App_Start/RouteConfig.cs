c20b37a52cc9f8df58708e3ce75d456e0c2f2e74
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2017-12-08 14:02:03 +0700

    gum
    gum

--- MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
+++ MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MVCLibraryproject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Librarian", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
