4dbd1cb82d72eb5602c3f171108c483da079c1ef
ploychanok <plo_y@live.com>
2017-12-11 10:37:17 +0700

    ลบ Upload File
    ไล่เอารูปออกให้หมด

--- MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
+++ MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace MVCLibraryproject
                 name: "Default",
                 url: "{controller}/{action}/{id}",
 
-                defaults: new { controller = "Librarian", action = "Service", id = UrlParameter.Optional }
+                defaults: new { controller = "Librarian", action = "LibEditProfile", id = UrlParameter.Optional }
 
             );
         }
