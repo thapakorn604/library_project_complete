19480b1b4d66a8a4c1d49cbcd3f17c40ac40f7c3
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2017-12-11 18:39:32 +0700

    Added some simple link

--- MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
+++ MVC-Library-project/MVC-Library-project/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace MVCLibraryproject
                 name: "Default",
                 url: "{controller}/{action}/{id}",
 
-                defaults: new { controller = "Librarian", action = "MemberManage", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 
             );
         }
