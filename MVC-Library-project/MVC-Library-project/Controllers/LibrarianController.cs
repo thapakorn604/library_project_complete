ee72ae198c7060b76b77d5461d05f98689d17fca
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2017-12-11 18:06:12 +0700

    just wanna commit

--- MVC-Library-project/MVC-Library-project/Controllers/LibrarianController.cs
+++ MVC-Library-project/MVC-Library-project/Controllers/LibrarianController.cs
@@ -12,41 +12,5 @@ namespace MVCLibraryproject.Controllers
         {
             return View();
         }
-        public ActionResult Service()
-        {
-            return View();
-        }
-        public ActionResult LibrarianProfile()
-        {
-            return View();
-        }
-        public ActionResult LibEditProfile()
-        {
-            return View();
-        }
-        public ActionResult History()
-        {
-            return View();
-        }
-        public ActionResult MemberRegister()
-        {
-            return View();
-        }
-        public ActionResult Book()
-        {
-            return View();
-        }
-        public ActionResult AddBook()
-        {
-            return View();
-        }
-        public ActionResult EditBook()
-        {
-            return View();
-        }
-        public ActionResult LibrarianHistory()
-        {
-            return View();
-        }
     }
 }
